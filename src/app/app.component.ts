c642390f84d228c8262ac8432bd9af6033d256bb
karimmoustamid <moustamid.teck@gmail.com>
2024-08-10 09:42:18 -0600

    Add DecimalPipe and update distance conversion display
    Included DecimalPipe in app.component imports for enhanced number formatting. Updated the conversion display in app.component.html to show miles as centimeters with specific formatting.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {CurrencyPipe, DatePipe, NgIf, TitleCasePipe} from "@angular/common";
+import {CurrencyPipe, DatePipe, DecimalPipe, NgIf, TitleCasePipe} from "@angular/common";
 import {ConvertPipe} from "./convert.pipe";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleCasePipe, DatePipe, CurrencyPipe, ConvertPipe, NgIf],
+  imports: [RouterOutlet, TitleCasePipe, DatePipe, CurrencyPipe, ConvertPipe, NgIf, DecimalPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
