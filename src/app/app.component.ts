47a2db63128c2b34934d5e02166924a7a1d812e4
karimmoustamid <moustamid.teck@gmail.com>
2024-08-10 09:31:46 -0600

    Add Bootstrap and enhance app component
    Integrated Bootstrap for consistent styling and improved UI. Expanded app component to support date, amount, and mileage inputs, displaying formatted outputs using Angular pipes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {TitleCasePipe} from "@angular/common";
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {CurrencyPipe, DatePipe, TitleCasePipe} from "@angular/common";
+import {ConvertPipe} from "./convert.pipe";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleCasePipe],
+  imports: [RouterOutlet, TitleCasePipe, DatePipe, CurrencyPipe, ConvertPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   name!: string;
+  date!: string;
+  amount!: number;
+  miles!: number;
 
-    onNameChange($event: Event) {
-        this.name = ($event.target as HTMLInputElement).value;
-    }
+  onNameChange($event: Event) {
+    this.name = ($event.target as HTMLInputElement).value;
+  }
+
+  onDateChange($event: Event) {
+    this.date = ($event.target as HTMLInputElement).value;
+  }
+
+  onAmountChange($event: Event){
+    this.amount = parseFloat(($event.target as HTMLInputElement).value);
+  }
+
+  onMilesChange($event: Event){
+    this.miles = parseFloat(($event.target as HTMLInputElement).value);
+  }
 }
